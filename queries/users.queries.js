25bea5624e4d6eec7d96a2f3a93d9b3557dce4c7
naolleg <naollegesse51@gmail.com>
2024-03-19 21:27:47 +0300

    some adjustment

--- queries/users.queries.js
+++
@@ -1,9 +0,0 @@
-const userquery={
-    getRoleByUsername:'SELECT role FROM users WHERE username = ?;',
-    getUserByusername:'SELECT * FROM users WHERE username = ?;',
-    getuserquery:'SELECT * FROM users',
-    registeruser:'INSERT INTO users (firstname, lastname, username, password, role,phonenumber,createdDate,status) VALUES (?, ?, ?, ?, ?, NOW(), 1);',
-    deactivateUser:'UPDATE users SET status = 0 WHERE user_id = ?;',
-    getmotherid:'SELECT mother_id FROM mother WHERE user_id = ?;'
-}
-module.exports=userquery
